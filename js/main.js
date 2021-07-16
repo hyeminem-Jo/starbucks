2d56b8702bb02fbbe7f3a63619421e3a55cb4a9f
hyeminem-Jo <konnimey@naver.com>
2021-07-16 20:16:57 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-// ---------------------------------------------------------
-// 상단의 search 버튼을 누를시 input이 옆으로 늘어나는 효과
-
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input')
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-})
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-})
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-})
 
 
 // ---------------------------------------------------------
@@ -181,9 +162,3 @@ new Swiper('.awards .swiper-container', {
     nextEl: '.awards .swiper-next'
   }
 })
-
-
-// ---------------------------------------------------------
-// Footer 년도 표시
-const thisYear = document.querySelector('.this-year')
-thisYear.textContent = new Date().getFullYear();
